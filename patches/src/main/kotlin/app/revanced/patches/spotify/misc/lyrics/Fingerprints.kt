8da0a5b6eff02a76b80d338b0434f18edfa3cbb6
LisaExists <lisaknoll07@gmail.com>
2026-08-28 19:41:45 +0200

    remove Spotify patches

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/lyrics/Fingerprints.kt
+++
@@ -1,7 +0,0 @@
-package app.revanced.patches.spotify.misc.lyrics
-
-import app.revanced.patcher.gettingFirstImmutableMethod
-import app.revanced.patcher.patch.BytecodePatchContext
-
-
-internal val BytecodePatchContext.httpClientBuilderMethod by gettingFirstImmutableMethod("client == null", "scheduler == null")
